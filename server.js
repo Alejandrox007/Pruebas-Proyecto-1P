8445701aa5765b117052017a1d44474246c8b374
LeonelTQL <leonel06tipan@gmail.com>
2026-07-23 15:50:21 -0500

    fix workflow

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Hospital API Server running on http://localhost:${PORT}`);
+  console.log(`Hospital API Servers running on http://localhost:${PORT}`);
 });
